7e9f277d9b5a4b76bb45092b522d05a9bd8ba2a2
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-22 08:35:09 +0400

    Initialized the database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
 const app = express()
 import mongoose from "mongoose"
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 try {
     mongoose.connect("mongodb://localhost:27017")
@@ -9,6 +9,10 @@ try {
 } catch (error) {
     if(error) throw error
 }
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World")
+})
 
 
 app.listen(port, () => console.log(`Listening at port ${port}`))
