79466d3eca075169d65387ee5b0bf26481664b98
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-07-13 23:21:50 +0400

    Created the user schema

---
+++ model/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { required } from "nodemon/lib/config";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String;
+        required : true
+    },
+    email : {
+        type: String,
+        required: [true, "Email is required"],
+        unique : true
+    },
+    password: {
+        type: String
+    },
+    {timestamps : true}
+})
+
+const user = mongoose.model("user", userSchema)
+
+export default user
