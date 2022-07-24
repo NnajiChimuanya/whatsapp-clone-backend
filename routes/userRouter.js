9b90b4d9e10e4e65e598b1c8a6ad96fd8681fa64
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-07-24 23:22:25 +0400

    auth

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -28,10 +28,17 @@ passport.use(
 );
 
 userRouter.get("/auth/success", (req, res) => {
-  res.status(200).json({
-    error: false,
-    user: req.user,
-  });
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({
+      error: true,
+      message: "Unathorized user",
+    });
+  }
 });
 
 userRouter.get(
